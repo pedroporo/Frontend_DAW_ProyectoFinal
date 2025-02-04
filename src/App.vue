2b2d114b7a3e6a6b59f45ff27aaa7bcf9b8f585f
IvanaPlamenovaMikinska <Ivana200123@gmail.com>
2025-02-04 17:29:49 +0100

    Merge Ivana

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,11 @@
 <script setup>
-<<<<<<< HEAD
 import { useRoute } from 'vue-router';
 
 const route = useRoute();
-=======
-import { RouterLink, RouterView } from 'vue-router'
->>>>>>> 63146574865b7177b6659e82bc0012db81fe0d96
 </script>
 
 <template>
   <header>
-<<<<<<< HEAD
     <div class="sidebar" v-if="route.path !== '/login'">
       <h1>Teleasistencia</h1>
       <nav>
@@ -23,20 +18,8 @@ import { RouterLink, RouterView } from 'vue-router'
         <RouterLink to="/">Gestión de Operadores</RouterLink>
         <RouterLink to="/">Cerrar Sesión</RouterLink>
       </nav>
-=======
-   
-    <div class="wrapper">
-
->>>>>>> 63146574865b7177b6659e82bc0012db81fe0d96
     </div>
   </header>
 
   <RouterView />
 </template>
-<<<<<<< HEAD
-=======
-
-<style scoped>
-
-</style>
->>>>>>> 63146574865b7177b6659e82bc0012db81fe0d96
