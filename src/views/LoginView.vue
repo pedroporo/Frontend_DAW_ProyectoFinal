e68ed1818eb22003916887df181d0850d70deaca
IvanaPlamenovaMikinska <Ivana200123@gmail.com>
2025-02-04 17:16:37 +0100

    Login

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -6,21 +6,13 @@ import '../assets/main.css';
 <template>
     <div class="content">
       <div class="container">
-        <form v-if="!isLogin" @submit.prevent="handleLogin">
+        <form @submit.prevent="handleLogin">
           <h2>Login</h2>
           <input type="text" v-model="username" placeholder="Usuario" class="input-field" required>
           <input type="password" v-model="password" placeholder="Contraseña" class="input-field" required>
           <button type="submit">Iniciar Sesión</button>
-          <p class="toggle">¿No tienes cuenta? <a href="#" @click="toggleForm">Regístrate</a></p>
-        </form>
-        <form v-else @submit.prevent="handleRegister">
-          <h2>Registro</h2>
-          <input type="text" v-model="newUsername" placeholder="Usuario" class="input-field" required>
-          <input type="email" v-model="email" placeholder="Correo Electrónico" class="input-field" required>
-          <input type="password" v-model="newPassword" placeholder="Contraseña" class="input-field" required>
-          <button type="submit">Registrarse</button>
-          <p class="toggle">¿Ya tienes cuenta? <a href="#" @click="toggleForm">Inicia sesión</a></p>
         </form>
       </div>
     </div>
   </template>
+ 
