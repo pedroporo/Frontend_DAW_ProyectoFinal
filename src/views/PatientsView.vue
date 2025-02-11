e4543d676a4aac4fdf9513696526199e0aefa2e0
FerriAchede <ferriyt38@gmail.com>
2025-02-11 18:47:21 +0100

    Agregar funcionalidad de búsqueda en la lista de pacientes por todos los campos.

--- src/views/PatientsView.vue
+++ src/views/PatientsView.vue
@@ -4,7 +4,30 @@ import { useStore } from '@/stores/patientStore';
 
 export default {
     computed: {
-        ...mapState(useStore, ['userNames', 'contactNames'])
+        ...mapState(useStore, ['userNames', 'contactNames']),
+        filteredPatients() {
+            return this.patients.filter(p => {
+                const searchLower = this.search.toLowerCase();
+                
+                return (
+                    p.name.toLowerCase().includes(searchLower) ||
+                    p.last_name.toLowerCase().includes(searchLower) ||
+                    p.birth_date.includes(searchLower) ||
+                    p.address.toLowerCase().includes(searchLower) ||
+                    p.city.toLowerCase().includes(searchLower) ||
+                    p.postal_code.includes(searchLower) ||
+                    p.dni.toLowerCase().includes(searchLower) ||
+                    p.health_card_number.toString().includes(searchLower) ||
+                    p.phone.includes(searchLower) ||
+                    p.email.toLowerCase().includes(searchLower) ||
+                    p.personal_situation.toLowerCase().includes(searchLower) ||
+                    p.health_situation.toLowerCase().includes(searchLower) ||
+                    p.housing_situation.toLowerCase().includes(searchLower) ||
+                    p.personal_autonomy.toLowerCase().includes(searchLower) ||
+                    p.economic_situation.toLowerCase().includes(searchLower)
+                );
+            });
+        }
     },
     methods: {
         ...mapActions(useStore, ['getPatients', 'createPatient', 'getContactsByPatientId']),
@@ -18,6 +41,7 @@ export default {
     data() {
         return {
             patients: [],
+            search: '',
         }
     },
     async mounted() {
@@ -29,7 +53,9 @@ export default {
 <template>
     <div class="content">
         <h2>Lista de Pacientes</h2>
-        <input type="text" class="form-control mb-3" placeholder="Buscar paciente...">
+        <div class="search-wrapper panel-heading col-sm-12"> 
+            <input type="text" v-model="search" class="form-control mb-3" placeholder="Buscar paciente...">
+        </div>
         <table class="table">
             <thead>
                 <tr>
@@ -41,7 +67,7 @@ export default {
                 </tr>
             </thead>
             <tbody>
-                <tr v-for="patient in patients" :key="patient.id">
+                <tr v-for="patient in filteredPatients" :key="patient.id">
                     <td>{{ patient.name + ' ' + patient.last_name }}</td>
                     <td>{{ "+34 " + patient.phone }}</td>
                     <td>{{ userNames(patient.user_id) || 'Sin asignar' }}</td>
