89d6cc6b3fd1625f97a95e1a3322c3180d54221c
FerriAchede <ferriyt38@gmail.com>
2025-02-13 15:58:35 +0100

    agrega eliminación automática de mensajes después de 12 segundos en el store de mensajes

--- src/stores/messagesStore.js
+++ src/stores/messagesStore.js
@@ -10,6 +10,14 @@ export const useMessagesStore = defineStore("messages", {
   actions: {
     addMessage(message, type = "info") {
       this.messages.push({ text: message, type });
+      setTimeout(() => {
+        const index = this.messages.findIndex(
+          (msg) => msg.text === message && msg.type === type
+        );
+        if (index !== -1) {
+          this.removeMessage(index);
+        }
+      }, 12000);
     },
     removeMessage(index) {
       this.messages.splice(index, 1);
