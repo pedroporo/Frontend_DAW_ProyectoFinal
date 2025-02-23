e1b0e9e36a78065d40960f6c8b180d26a16d634d
Ferran <ferriyt38@gmail.com>
2025-02-23 20:58:42 +0100

    actualiza el manejo de usuario en el store de login y añade almacenamiento de datos del usuario al iniciar sesión

--- src/stores/loginStore.js
+++ src/stores/loginStore.js
@@ -1,14 +1,14 @@
 import { defineStore, mapActions } from "pinia";
 import axios from "axios";
 import { useMessagesStore } from "./messagesStore";
-import router from "@/router"; // Importa el router
+import router from "@/router";
 import api from "./api/axiosInstance";
 const urlLoginGoogle = import.meta.env.VITE_API_LOGIN_GOOGLE_URL;
 const urlNormal = import.meta.env.VITE_API_BASE_URL;
 
 export const useLoginStore = defineStore("login", {
   state: () => ({
-    userLogged: {},
+    user: {},
     token: null,
   }),
   actions: {
@@ -29,6 +29,7 @@ export const useLoginStore = defineStore("login", {
           this.user = data.data.user;
           this.token = data.data.token;
           localStorage.setItem("auth_token", data.data.token);
+          localStorage.setItem("userData", JSON.stringify(this.user));
     
           router.push("/");
         }
@@ -38,15 +39,18 @@ export const useLoginStore = defineStore("login", {
     },
 
     async logout() {
-      const response = await api.post(`${urlNormal}logout`);
-    
-      //console.log(`${urlNormal}logout`);
-      console.log(response);
+      try {
+        const response = await api.post(`${urlNormal}logout`);
+        
+        this.user = {};
+        this.token = null;
 
-      this.user = {};
-      this.token = null;
-
-      localStorage.removeItem("auth_token");
+        localStorage.removeItem("auth_token");
+        localStorage.removeItem("userData");
+        return response.data;
+      } catch (error) {
+        this.addMessage("Error al cerrar sesión" + error, "error");
+      }
     },
   },
 });
